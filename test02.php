8a9201afda520dd7e3414deed837b0b5f0d97481
HOSHINOYUYA <125641044+HOSHINOYUYA@users.noreply.github.com>
2024-03-08 04:28:56 +0800

    Add files via upload

---
+++ test02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    
+    echo "U";
+    echo "S<br/>";
+    echo "U<br/>S<br/>";
+
+    $a = 2;
+    $b = 3;
+
+    if($a > $b){
+        echo "a > b";
+    }else{
+        echo "b > a";
+    }
+    
+    $c = $a*$b;
+
+    echo "<br/>$c";
+
+    
+?>
+
+</html>
